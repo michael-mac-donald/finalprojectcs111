fa9a4a8529fdeb15f50ad5591ad77e40fe173b05
Michael MacDonald <mikemac2005@gmail.com>
2023-11-27 17:06:31 -0500

    bunch of functions this is taking so fucking long

--- FinalProject/CaptureTheFlag.py
+++ FinalProject/CaptureTheFlag.py
@@ -5,11 +5,15 @@
 
 from sac_graphics import *
 import time
-
+from game import Game
 def main():
-    w, h = 1200, 800
+    w, h = 1400, 800
     win = GraphWin("Capture the Flag", w, h)
     win.setBackground(color_rgb(220,220,220))
+    run = True
+    game = Game(win, w, h)
+
+    game.drawgame()
 
     playbutton = Rectangle(Point(w/2 + 100, 150), Point(w/2 - 100, 225))
     playbutton.setFill(color_rgb(145, 153, 255))
@@ -41,10 +45,38 @@ def main():
     creditstext.setFace("courier")
     creditstext.setSize(24)
 
+    # drawmenu(playbutton, infobutton, creditsbutton, playtext, infotext, creditstext, win, w)
+
+    print(win.getMouse())
+
+    # while run:
+    #     mc = win.checkMouse()
+    #     if mc:
+    #         if isClicked(mc, playbutton):
+    #            # close this window and open the game window
+    #            win.close()
+    #         elif isClicked(mc, infobutton):
+    #               undrawmenu(playbutton, infobutton, creditsbutton, playtext, infotext, creditstext)
+    #             #display infobutton text
+    #         elif isClicked(mc, creditsbutton):
+    #               undrawmenu(playbutton, infobutton, creditsbutton, playtext, infotext, creditstext)
+    #             #display credits text
+
+def isClicked(Point, rect) -> bool:
+    px, py = Point.getX(), Point.getY()
+    p1, p2 = rect.getP1(), rect.getP2()
+    if px >= p1.getX() and px <= p2.getX() and py >= p1.getY() and py <= p2.getY():
+        return True
+    else:
+        return False
+
+
+def drawmenu(playbutton, infobutton, creditsbutton, playtext, infotext, creditstext, win, w):
     for i in range(1, 50):
         file = "assets/title/frame-" + str(i) + ".png"
         title = Image((Point(w / 2, 50)), file)
         title.draw(win)
+        time.sleep(.02)
 
     playbutton.draw(win)
     time.sleep(.2)
@@ -58,7 +90,14 @@ def main():
     time.sleep(.2)
     creditstext.draw(win)
 
-    print(win.getMouse())
+def undrawmenu(playbutton, infobutton, creditsbutton, playtext, infotext, creditstext):
+    playbutton.undraw()
+    infobutton.undraw()
+    creditsbutton.undraw()
+    playtext.undraw()
+    infotext.undraw()
+    creditstext.undraw()
+
 
 
 if __name__ == "__main__":
---
+++ FinalProject/game.py
@@ -0,0 +1,92 @@
+# Michael MacDonald and Joey Sullivan
+# Final Project - Capture The Flag
+# Game
+# Game Classes for Capture The Flag Game
+
+from sac_graphics import *
+import time
+
+class Game:
+
+    def __init__(self, win, w, h):
+        self.win = win
+        self.blueside = Rectangle(Point(0, h), Point(w / 2, 0))
+        self.blueside.setFill(color_rgb(145, 153, 255))
+
+        self.redside = Rectangle(Point(w / 2, h), Point(w, 0))
+        self.redside.setFill(color_rgb(255, 153, 142))
+
+        self.bplayer = Circle(Point(w / 4, h / 2), 22)
+        self.bplayer.setFill("blue")
+        self.bplayer.setOutline(color_rgb(0, 13, 128))
+        self.bplayer.setWidth(4)
+
+        self.rplayer = Circle(Point(w * 3 / 4, h / 2), 22)
+        self.rplayer.setFill("red")
+        self.rplayer.setOutline(color_rgb(128, 13, 0))
+        self.rplayer.setWidth(4)
+
+        self.bsafe = Rectangle(Point(0, h), Point(w / 15, 0))
+        self.bsafe.setFill(color_rgb(255, 198, 191))
+        self.bsafe.setWidth(1)
+
+        self.rsafe = Rectangle(Point(w, h), Point(w * 14 / 15, 0))
+        self.rsafe.setFill(color_rgb(191, 198, 255))
+        self.rsafe.setWidth(1)
+
+        self.rflag = Image(Point(1200, 400), "assets/flags/rflag.png")
+        self.bflag = Image(Point(200, 400), "assets/flags/bflag.png")
+
+        self.rsafezonetext = Image(Point(50, 415), "assets/rsafezone.png")
+
+        self.bsafezonetext = Image(Point(1350, 385), "assets/bsafezone.png")
+
+
+    def drawgame(self):
+
+        self.blueside.draw(self.win)
+        self.redside.draw(self.win)
+        self.bplayer.draw(self.win)
+        self.rplayer.draw(self.win)
+        self.bsafe.draw(self.win)
+        self.rsafe.draw(self.win)
+        self.rflag.draw(self.win)
+        self.bflag.draw(self.win)
+        self.rsafezonetext.draw(self.win)
+        self.bsafezonetext.draw(self.win)
+
+    def undrawgame(self):
+
+        self.blueside.undraw()
+        self.redside.undraw()
+        self.bplayer.undraw()
+        self.rplayer.undraw()
+        self.bsafe.undraw()
+        self.rsafe.undraw()
+        self.rflag.undraw()
+        self.bflag.undraw()
+        self.rsafezonetext.undraw()
+        self.bsafezonetext.undraw()
+
+    # def move(self):
+    #     flag = True
+    #     while flag:
+    #         checkKey = win.checkKey
+    #
+    #         if checkKey == "w":
+    #
+    #
+    #         elif checkKey == "a":
+    #
+    #
+    #         elif checkKey == "s":
+    #
+    #
+    #         elif checkKey == "d":
+
+
+
+
+
+    # def maps(self):
+#         map selector, puts black rectangles on the map that have collision detection
